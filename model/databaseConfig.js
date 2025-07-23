1f624168dcbf647d6bd969b914780e3b92e56b4b
v1binn <vibinsebastian07@gmail.com>
2025-07-23 13:08:14 +0800

    Fixed verification and added gitignore

--- model/databaseConfig.js
+++ model/databaseConfig.js
@@ -2,10 +2,10 @@ var mysql = require('mysql2');
 var dbconnect = {
     getConnection: function () {
         var conn = mysql.createConnection({
-            host: "localhost",
-            user: "root",
-            password: "ubuntu123",
-            database: "islandfurniture-it07"
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password:process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE
         });
         return conn;
     }
